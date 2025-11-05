2e44ae722f3fdba0371f6e7124ca25cfdacad913
Andreas <andreas.reinhol@gmail.com>
2025-11-05 10:52:34 +0100

    api call for weather data

---
+++ weather_api_call.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import pandas as pd
+from datetime import datetime, timedelta
+
+def get_weather(lat=59.33, lon=18.06):
+
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": "temperature_2m",
+        "timezone": "Europe/Stockholm"
+    }
+
+    respons = requests.get(url, params=params).json()
+
+    # count = 0
+    # time_data = []
+    # temp_data = []
+    # while count < 24:
+    #     time_data.append(respons["hourly"]["time"][count])
+    #     temp_data.append(respons["hourly"]["temperature_2m"][count])
+
+    #     count += 1
+
+    # data = {"Time": time_data,
+    #         "Temperature": temp_data}
+
+    df = pd.DataFrame({
+    "time": pd.to_datetime(respons["hourly"]["time"]),
+    "temperature": respons["hourly"]["temperature_2m"]
+    })
+ 
+ 
+    now = datetime.now()
+    next_24h = now + timedelta(hours=24)
+    df_24 = df[(df["time"] >= now) & (df["time"] <= next_24h)].reset_index()
+    df_24 = df_24.drop(["index"], axis=1)
+
+    return df_24
+
+
+if __name__ == "__main__":
+    get_weather()
