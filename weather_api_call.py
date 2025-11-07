8aa6e1d9ab65c0cb6aef5fc7e31572bbe51e2abe
Andreas <andreas.reinhol@gmail.com>
2025-11-07 10:11:05 +0100

    added try and except to api call to catch errors

--- weather_api_call.py
+++ weather_api_call.py
@@ -12,23 +12,25 @@ def get_weather(lat=59.33, lon=18.06):
         "hourly": "temperature_2m",
         "timezone": "Europe/Stockholm"
     }
+    try:
+        respons = requests.get(url, params=params).json()
 
-    respons = requests.get(url, params=params).json()
 
+        df = pd.DataFrame({
+        "time": pd.to_datetime(respons["hourly"]["time"]),
+        "temperature": respons["hourly"]["temperature_2m"]
+        })
+    
+    
+        now = datetime.now()
+        next_24h = now + timedelta(hours=24)
+        df_24 = df[(df["time"] >= now) & (df["time"] <= next_24h)].reset_index()
+        df_24 = df_24.drop(["index"], axis=1)
 
-    df = pd.DataFrame({
-    "time": pd.to_datetime(respons["hourly"]["time"]),
-    "temperature": respons["hourly"]["temperature_2m"]
-    })
- 
- 
-    now = datetime.now()
-    next_24h = now + timedelta(hours=24)
-    df_24 = df[(df["time"] >= now) & (df["time"] <= next_24h)].reset_index()
-    df_24 = df_24.drop(["index"], axis=1)
-
-    return df_24
-
+        return df_24
+    
+    except Exception as issue:
+        return issue
 
 if __name__ == "__main__":
     get_weather()
